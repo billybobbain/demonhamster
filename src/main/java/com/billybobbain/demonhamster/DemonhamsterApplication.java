2d1fc5ffde154e3eb790087ec5ab85a5b34d28ae
billybobbain <billybobbain@gmail.com>
2021-03-13 12:55:23 -0600

    generated project

---
+++ src/main/java/com/billybobbain/demonhamster/DemonhamsterApplication.java
@@ -0,0 +1,13 @@
+package com.billybobbain.demonhamster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonhamsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonhamsterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/billybobbain/demonhamster/DemonhamsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billybobbain.demonhamster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonhamsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
